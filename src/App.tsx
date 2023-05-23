854c6365d0146dc9f02bc5b2bd353e0398da5ed7
Caio Bastos <caio.bastos@desbravador.com.br>
2023-05-23 14:45:58 -0300

    #1 commit home tela do pikachu - falta button para a pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './components/navbar/index'
 import Footer from './components/footer/index'
+import Home from './components/home/index'
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Footer/>
+       {/* <Navbar/> */}
+      {/* <Footer/>  */}
+      <Home/>
     </>
   )
 }
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import styles from './index.module.css'
-import Pika from '../../assets/images/BannerComplete.png';
+import Pika from '../../../public/assets/images/BannerComplete.png';
 
 const Home: React.FC = () => {
     return (
         <home className={styles.home}>
-            <img src={Pika} className={styles.pika}/>
+            <div className={styles.banner}>
+                <div className={styles.banner}>
+                    <h1 className={styles.titulo}>
+                        <strong>Find</strong> all your
+                        <br/>favorite<br/>
+                        <strong>Pokemon</strong>
+                    </h1>
+                    <h2 className={styles.subtitulo}>
+                        You can know the type of Pokemon,<br/> its strengths, disadvantages and<br/> abilities
+                    </h2>
+                    <img src={Pika} className={styles.pika}/>
+                </div>
+            </div>
         </home>
     );
 };
